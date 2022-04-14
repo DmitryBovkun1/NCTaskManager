a08182b51a8c475837d5b53662d45f51cd5700d7
Dmitry_Bovkun <dimon4ikbovkun@gmail.com>
2022-04-14 12:20:29 +0300

    practice_8.1

--- src/main/java/ua/edu/sumdu/j2se/bovkun/tasks/AbstractTaskList.java
+++ src/main/java/ua/edu/sumdu/j2se/bovkun/tasks/AbstractTaskList.java
@@ -10,13 +10,7 @@ public abstract class AbstractTaskList implements Iterable<Task> {
     public final AbstractTaskList incoming(int from, int to)
     {
         AbstractTaskList resultList = TaskListFactory.createTaskList(ListTypes.getTypeList(this));
-        for(int i = 0; i < size(); i++)
-        {
-            if(getTask(i).nextTimeAfter(from) != -1 && getTask(i).nextTimeAfter(from) <= to)
-            {
-                resultList.add(getTask(i));
-            }
-        }
+        this.getStream().filter(task -> task.nextTimeAfter(from) != -1 && task.nextTimeAfter(from) <= to).forEach(resultList::add);
         return resultList;
     }
 
